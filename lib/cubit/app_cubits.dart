31c83f66a2f9012b901307e34be1e601779a14b4
Ionut Balica <balicaionut@gmail.com>
2022-02-05 15:29:10 +0200

    test

---
+++ lib/cubit/app_cubits.dart
@@ -0,0 +1,17 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class CubitStates extends Equatable {}
+
+class InitialState extends CubitStates {
+  @override
+  // TODO: implement props
+  List<Object> get props => [];
+}
+
+class WelcomeState extends CubitStates {
+  @override
+  // TODO: implement props
+  List<Object> get props => [];
+}
